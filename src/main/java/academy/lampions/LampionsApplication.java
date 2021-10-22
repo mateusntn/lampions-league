2cf2eabe0d6e7fe00dae6582746520965b45f3fa
mateusntn <mateusantoniodev@gmail.com>
2021-10-22 18:41:45 -0300

    first commit

---
+++ src/main/java/academy/lampions/LampionsApplication.java
@@ -0,0 +1,13 @@
+package academy.lampions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LampionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LampionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/lampions/LampionsApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.lampions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LampionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
