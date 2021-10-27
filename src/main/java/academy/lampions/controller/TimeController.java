26a1e90d1e414bf2f131d05764b8c1bc4e69fcc2
mateusntn <mateusantoniodev@gmail.com>
2021-10-26 23:16:08 -0300

    Create update method to Time

--- src/main/java/academy/lampions/controller/TimeController.java
+++ src/main/java/academy/lampions/controller/TimeController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +30,7 @@ public class TimeController {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Time> getById(@PathVariable int id) {
+    public ResponseEntity<Time> getById(@PathVariable Integer id) {
         Time time = service.findById(id);
         return ResponseEntity.ok().body(time);
     }
@@ -39,5 +40,11 @@ public class TimeController {
         service.create(time);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(time.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Time> update(@PathVariable Integer id, @RequestBody Time time) {
+        Time novoTime = service.update(id, time);
+        return ResponseEntity.ok().body(new Time(novoTime));
     }
 }
--- src/main/java/academy/lampions/service/TimeService.java
+++ src/main/java/academy/lampions/service/TimeService.java
@@ -18,13 +18,28 @@ public class TimeService {
         return repository.findAll();
     }
 
-    public Time findById(int id) {
+    public Time findById(Integer id) {
         Optional<Time> time = repository.findById(id);
         return time.orElse(null);
     }
 
     public Time create(Time time) {
         time.setId(null);
+        return repository.save(time);
+    }
+
+    public Time update(Integer id, Time novoTime) {
+        Time time = findById(id);
+
+        time.setNome(novoTime.getNome() != null ? novoTime.getNome() : time.getNome());
+        time.setDataFundacao(novoTime.getDataFundacao() != null ? novoTime.getDataFundacao() : time.getDataFundacao());
+        time.setTitulos(novoTime.getTitulos() != null ? novoTime.getTitulos() : time.getTitulos());
+        time.setEscudo(novoTime.getEscudo() != null ? novoTime.getEscudo() : time.getEscudo());
+        time.setPontos(novoTime.getPontos() != null ? novoTime.getPontos() : time.getPontos());
+        time.setVitorias(novoTime.getVitorias() != null ? novoTime.getVitorias() : time.getVitorias());
+        time.setEmpates(novoTime.getEmpates() != null ? novoTime.getEmpates() : time.getEmpates());
+        time.setDerrotas(novoTime.getDerrotas() != null ? novoTime.getDerrotas() : time.getDerrotas());
+        
         return repository.save(time);
     }
 }
