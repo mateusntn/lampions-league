8b9a34f6f585770b5b8f625d7725cdb0c4d6434c
mateusntn <mateusantoniodev@gmail.com>
2021-10-30 10:35:02 -0300

    findAll in team-read component

--- lampions-front/src/app/app.module.ts
+++ lampions-front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { TeamReadComponent } from './components/views/team/team-read/team-read.c
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
