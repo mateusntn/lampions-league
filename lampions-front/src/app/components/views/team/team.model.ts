cbc716c9fdb761114f39f4ba09e997d66e99fee1
mateusntn <mateusantoniodev@gmail.com>
2021-10-30 10:34:13 -0300

    Create team model

---
+++ lampions-front/src/app/components/views/team/team.model.ts
@@ -0,0 +1,11 @@
+export interface Team {
+  id?: String;
+  name: String;
+  foudationDate: String;
+  titles: String;
+  emblem: String;
+  points: String;
+  victories: String;
+  draws: String;
+  defeats: String;
+}
