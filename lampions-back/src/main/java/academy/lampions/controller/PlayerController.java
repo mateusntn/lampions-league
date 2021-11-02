5f324f23ed0851a62e4e4502500e735603a17669
mateusntn <mateusantoniodev@gmail.com>
2021-11-01 22:59:57 -0300

    Remove unused imports

--- lampions-back/src/main/java/academy/lampions/controller/PlayerController.java
+++ lampions-back/src/main/java/academy/lampions/controller/PlayerController.java
@@ -1,7 +1,6 @@
 package academy.lampions.controller;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
--- lampions-back/src/main/java/academy/lampions/repository/PlayerRepository.java
+++ lampions-back/src/main/java/academy/lampions/repository/PlayerRepository.java
@@ -3,7 +3,6 @@ package academy.lampions.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import academy.lampions.entity.Player;
